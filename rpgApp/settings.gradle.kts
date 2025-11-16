5f8d4e63ed887dbde10bc0c9d42aa6d56be3acf2
Laura Ramos <164506063+lauramoskk@users.noreply.github.com>
2025-11-16 17:32:21 -0300

    Implementando services e notifications

--- rpgApp/settings.gradle.kts
+++ rpgApp/settings.gradle.kts
@@ -1,24 +1,15 @@
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    plugins {
+        id("com.android.application") version "8.13.0"
+        id("org.jetbrains.kotlin.android") version "2.2.20"
+        id("org.jetbrains.kotlin.kapt") version "2.2.20"
+        id("com.google.devtools.ksp") version "1.9.24-1.0.20"
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositories { google(); mavenCentral() }
 }
-
 rootProject.name = "My Application"
 include(":app")
- 
